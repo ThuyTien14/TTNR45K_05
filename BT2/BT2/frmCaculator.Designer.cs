5c3a22a21dff3a7612df414a8ea751f25d27f9a4
Tien14 <tranvothithuytien1411@gmail.com>
2021-06-12 10:33:26 +0700

    send

---
+++ BT2/BT2/frmCaculator.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace BT2
+{
+    partial class frmCaculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSo1 = new System.Windows.Forms.TextBox();
+            this.txtSo2 = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.bntCong = new System.Windows.Forms.Button();
+            this.bntTru = new System.Windows.Forms.Button();
+            this.bntNhan = new System.Windows.Forms.Button();
+            this.bntChia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSo1
+            // 
+            this.txtSo1.Location = new System.Drawing.Point(61, 51);
+            this.txtSo1.Name = "txtSo1";
+            this.txtSo1.Size = new System.Drawing.Size(100, 20);
+            this.txtSo1.TabIndex = 0;
+            // 
+            // txtSo2
+            // 
+            this.txtSo2.Location = new System.Drawing.Point(330, 50);
+            this.txtSo2.Name = "txtSo2";
+            this.txtSo2.Size = new System.Drawing.Size(100, 20);
+            this.txtSo2.TabIndex = 1;
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(202, 117);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.Size = new System.Drawing.Size(100, 20);
+            this.txtKQ.TabIndex = 2;
+            // 
+            // bntCong
+            // 
+            this.bntCong.Location = new System.Drawing.Point(37, 171);
+            this.bntCong.Name = "bntCong";
+            this.bntCong.Size = new System.Drawing.Size(75, 23);
+            this.bntCong.TabIndex = 3;
+            this.bntCong.Text = "Cộng";
+            this.bntCong.UseVisualStyleBackColor = true;
+            this.bntCong.Click += new System.EventHandler(this.bntCong_Click);
+            // 
+            // bntTru
+            // 
+            this.bntTru.Location = new System.Drawing.Point(152, 171);
+            this.bntTru.Name = "bntTru";
+            this.bntTru.Size = new System.Drawing.Size(75, 23);
+            this.bntTru.TabIndex = 4;
+            this.bntTru.Text = "Trừ";
+            this.bntTru.UseVisualStyleBackColor = true;
+            this.bntTru.Click += new System.EventHandler(this.bntTru_Click);
+            // 
+            // bntNhan
+            // 
+            this.bntNhan.Location = new System.Drawing.Point(270, 171);
+            this.bntNhan.Name = "bntNhan";
+            this.bntNhan.Size = new System.Drawing.Size(75, 23);
+            this.bntNhan.TabIndex = 5;
+            this.bntNhan.Text = "Nhân";
+            this.bntNhan.UseVisualStyleBackColor = true;
+            this.bntNhan.Click += new System.EventHandler(this.bntNhan_Click);
+            // 
+            // bntChia
+            // 
+            this.bntChia.Location = new System.Drawing.Point(383, 171);
+            this.bntChia.Name = "bntChia";
+            this.bntChia.Size = new System.Drawing.Size(75, 23);
+            this.bntChia.TabIndex = 6;
+            this.bntChia.Text = "Chia";
+            this.bntChia.UseVisualStyleBackColor = true;
+            this.bntChia.Click += new System.EventHandler(this.bntChia_Click);
+            // 
+            // frmCaculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 261);
+            this.Controls.Add(this.bntChia);
+            this.Controls.Add(this.bntNhan);
+            this.Controls.Add(this.bntTru);
+            this.Controls.Add(this.bntCong);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtSo2);
+            this.Controls.Add(this.txtSo1);
+            this.Name = "frmCaculator";
+            this.Text = "frmCaculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSo1;
+        private System.Windows.Forms.TextBox txtSo2;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.Button bntCong;
+        private System.Windows.Forms.Button bntTru;
+        private System.Windows.Forms.Button bntNhan;
+        private System.Windows.Forms.Button bntChia;
+    }
+}
+
---
+++ BT2/BT2/frmCaculator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BT2
+{
+    public partial class frmCaculator : Form
+    {
+        public frmCaculator()
+        {
+            InitializeComponent();
+        }
+
+        private void bntCong_Click(object sender, EventArgs e)
+        {
+            string sSo1 = txtSo1.Text;
+            decimal dSo1 = Convert.ToDecimal(sSo1);
+            string sSo2 = txtSo2.Text;
+            decimal dSo2 = Convert.ToDecimal(sSo2);
+            decimal dKQ = dSo1 + dSo2;
+            txtKQ.Text = dKQ.ToString();
+        }
+
+        private void bntTru_Click(object sender, EventArgs e)
+        {
+            string sSo1 = txtSo1.Text;
+            decimal dSo1 = Convert.ToDecimal(sSo1);
+            string sSo2 = txtSo2.Text;
+            decimal dSo2 = Convert.ToDecimal(sSo2);
+            decimal dKQ = dSo1 - dSo2;
+            txtKQ.Text = dKQ.ToString();
+        }
+
+        private void bntNhan_Click(object sender, EventArgs e)
+        {
+            string sSo1 = txtSo1.Text;
+            decimal dSo1 = Convert.ToDecimal(sSo1);
+            string sSo2 = txtSo2.Text;
+            decimal dSo2 = Convert.ToDecimal(sSo2);
+            decimal dKQ = dSo1 * dSo2;
+            txtKQ.Text = dKQ.ToString();
+        }
+
+        private void bntChia_Click(object sender, EventArgs e)
+        {
+            string sSo1 = txtSo1.Text;
+            decimal dSo1 = Convert.ToDecimal(sSo1);
+            string sSo2 = txtSo1.Text;
+            decimal dSo2 = Convert.ToDecimal(sSo2);
+            decimal dKQ = dSo1 / dSo2;
+            txtKQ.Text = dKQ.ToString();
+        }
+    }
+}
